be3ec0b7f0036cbd9e5651e1e58ace7d59bf3405
Eduardo Arango <carlos.arango.gutierrez@correounivalle.edu.co>
2016-11-29 17:46:42 -0500

    Update fedora_fenics_deps.sh

--- fedora/fedora_fenics_deps.sh
+++ fedora/fedora_fenics_deps.sh
@@ -26,6 +26,13 @@ dnf -y install \ htop git wget gcc gfortran openblas cmake gcc-c++
 \libcurl libcurl-devel libstdc++ libstdc++-devel boost boost-devel
 dnf -y update
 
+# Taken from https://fenicsproject.org/download/installation_using_hashdist.html#installation-using-hashdist
+git clone https://bitbucket.org/fenics-project/fenics-developer-tools.git
+cd fenics-developer-tools
+cp install/profiles/fenics.Linux.yaml fenics.yaml
+# vi fenics.yaml  # Uncommento to modify the profile if needed 
+./install/fenics-install.sh fenics.yaml
+
 echo "Now you can install Fenics on your Fedora"
 echo "https://fenicsproject.org/index.html"
 echo "How do you fedora?"
